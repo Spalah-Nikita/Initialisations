a0034f04eed154251a481814fca32f0a36f159ab
Nikita Vintonovich <nikita.vintonovich@gmail.com>
2017-09-06 10:25:14 +0300

    Добавил пустой проект

---
+++ InitializatoinExamples/InitializatoinExamples/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InitializatoinExamples
+//
+//  Created by Nikita Vintonovich on 9/6/17.
+//  Copyright © 2017 Nikita Vintonovich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
